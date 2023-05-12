4bcc67b00545f782476d99957e74e134bd3f0ab6
Julian Beldotti <julian.beldotti@gmail.com>
2023-05-11 23:59:08 -0500

    Feat: Add MVC flow and refactor to create a link that sorts cats alphabetically

--- app/controllers/grannies/cats_controller.rb
+++ app/controllers/grannies/cats_controller.rb
@@ -1,6 +1,7 @@
 class Grannies::CatsController < ApplicationController
   def index
     @granny = Granny.find(params[:id])
+    @cats = @granny.sort_aplha(params['sort'])
   end
   
   def new
@@ -14,8 +15,15 @@ class Grannies::CatsController < ApplicationController
     redirect_to "/grannies/#{granny.id}/cats"
   end
 
+  
   private
   def cat_params
     params.permit(:name, :spayed_neutered, :lives)
   end
+  
+  # def sort_aplha
+  #   granny = Granny.find(params[:id])
+  #   cat = granny.cats.find(params[:id])
+  #   cat.order(:name)
+  # end
 end
